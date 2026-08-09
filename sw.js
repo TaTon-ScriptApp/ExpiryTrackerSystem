d031d396a5f23c6f4b9622c32a5e1bf2c2d91896
TaTon-ScriptApp <iton2542@gmail.com>
2026-08-09 17:56:23 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', (e) => {
-  // บังคับดึงข้อมูลใหม่จากเซิร์ฟเวอร์เสมอ ไม่ใช้ความจำเก่า
-  e.respondWith(fetch(e.request));
-});
